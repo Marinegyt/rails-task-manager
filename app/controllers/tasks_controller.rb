bed33372600bebe779bfb863bd4c1af13fef0c90
Marine Gayet <marinegayet@gmail.com>
2024-02-20 13:03:36 +0100

    make tasks controller index show new edit

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,45 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    redirect_to tasks_path(@task)
+  end
+
+  def edit
+
+  end
+
+  def update
+    @task.update(task_params)
+
+    redirect_to task_path(@task)
+  end
+
+  def show
+
+  end
+
+  private
+
+  def set_task
+    # Cette méthode privée me permet de refactoriser en ne répétant pas la même ligne dans plusieurs méthodes
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    # Cette méthode gère les STRONG PARAMS => elle autorise sur le model restaurant l'accès aux params name, address et ratings
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+end
